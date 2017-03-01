ac31d549536cb9b4da59ff0d4ba5a9faee859460
Yubo Zhi <normanzyb@gmail.com>
2017-03-01 02:55:59 +0000

    examples/simple: Unrolled find neighbors loop to reduce memory operations

--- examples/simple/src/Mesh.cpp
+++ examples/simple/src/Mesh.cpp
@@ -47,77 +47,52 @@ size_t Mesh::getNeighbors(size_t i, size_t *n)
 
 size_t Mesh::find6Neighbors( uint x, uint y, uint z, size_t *neighbors) 
 {
-	uint nx[6],ny[6],nz[6];
-	
-	for (uint i = 0; i < 6; i++) {
-		nx[i] = x;
-		ny[i] = y;
-		nz[i] = z;
-	}
-	
-	//first 6 neighbors
-	nx[0] -= 1;
-	ny[1] -= 1;
-	nz[2] -= 1;
-	nx[3] += 1;
-	ny[4] += 1;
-	nz[5] += 1;
-	
 	size_t size = 0;
-	for (uint i = 0; i < 6; i++) {
-		if (nx[i] >= data.size[0]) continue;	
-		if (ny[i] >= data.size[1]) continue;	
-		if (nz[i] >= data.size[2]) continue;	
-	
-		*neighbors++ = data.convertIndex(nx[i],ny[i],nz[i]);
-		size++;
-	}
+	auto check = [=, &size, &neighbors](uint x, uint y, uint z) {
+		if (x < data.size[0] && y < data.size[1] && z < data.size[2]) {
+			*neighbors++ = data.convertIndex(x, y, z);
+			size++;
+		}
+	};
+
+	check(x - 1, y, z);
+	check(x, y - 1, z);
+	check(x, y, z - 1);
+	check(x + 1, y, z);
+	check(x, y + 1, z);
+	check(x, y, z + 1);
 	return size;
 }
 
 size_t Mesh::find18Neighbors( uint x, uint y, uint z, size_t *neighbors) 
 {
-	uint nx[18],ny[18],nz[18];
-	
-	for (uint i = 0; i < 18; i++) {
-		nx[i] = x;
-		ny[i] = y;
-		nz[i] = z;
-	}
-	
-	//first 6 neighbors
-	nx[0] -= 1;
-	ny[1] -= 1;
-	nz[2] -= 1;
-	nx[3] += 1;
-	ny[4] += 1;
-	nz[5] += 1;
-	
-	//the rest of the 18
-	nx[6] -= 1; ny[6]  -= 1;
-	nx[7] += 1; ny[7]  -= 1;
-	ny[8] -= 1; nz[8]  -= 1;
-	ny[9] += 1; nz[9]  -= 1;
-	nz[10]-= 1; nx[10] -= 1;
-	nz[11]+= 1; nx[11] -= 1;
-		
-	nx[12] -= 1; ny[12] += 1;
-	nx[13] += 1; ny[13] += 1;
-	ny[14] -= 1; nz[14] += 1;
-	ny[15] += 1; nz[15] += 1;
-	nz[16] -= 1; nx[16] += 1;
-	nz[17] += 1; nx[17] += 1;
-	
 	size_t size = 0;
-	for (uint i = 0; i < 18; i++) {
-		
-		
-		if (nx[i] >= data.size[0]) continue;	
-		if (ny[i] >= data.size[1]) continue;	
-		if (nz[i] >= data.size[2]) continue;	
-	
-		*neighbors++ = data.convertIndex(nx[i],ny[i],nz[i]);
-		size++;
-	}
+	auto check = [=, &size, &neighbors](uint x, uint y, uint z) {
+		if (x < data.size[0] && y < data.size[1] && z < data.size[2]) {
+			*neighbors++ = data.convertIndex(x, y, z);
+			size++;
+		}
+	};
+
+	check(x - 1, y, z);
+	check(x, y - 1, z);
+	check(x, y, z - 1);
+	check(x + 1, y, z);
+	check(x, y + 1, z);
+	check(x, y, z + 1);
+
+	check(x - 1, y - 1, z);
+	check(x - 1, y + 1, z);
+	check(x, y - 1, z - 1);
+	check(x, y - 1, z + 1);
+	check(x - 1, y, z - 1);
+	check(x + 1, y, z - 1);
+
+	check(x + 1, y - 1, z);
+	check(x + 1, y + 1, z);
+	check(x, y + 1, z - 1);
+	check(x, y + 1, z + 1);
+	check(x - 1, y, z + 1);
+	check(x + 1, y, z + 1);
 	return size;
 }
